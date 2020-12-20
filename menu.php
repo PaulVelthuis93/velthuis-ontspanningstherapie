cd475de82aeb329c4d6ec2f21f50060a5e9e08d6
Paul Velthuis <pvelthuis@ZALANDO-33380.corp.ad.zalando.net>
2020-12-20 10:44:12 +0100

    first website

---
+++ menu.php
@@ -0,0 +1,33 @@
+ echo "<ul id="nav"><nav>";
+echo	"<li>";
+echo		"<a href="index.html">Home</a>";
+echo	"</li>";
+
+echo		"<li>";
+echo			"<a href="José-Velthuis.html">José Velthuis</a>";
+echo		"</li>";
+echo		"<li onClick=”return true”>Voor wie?<ul>";
+echo				"<li><a href="stress.html">Stress</a ></li>";
+echo				"<li><a href="doelgroep.html">Doelgroep</a></li>";
+echo			"</ul>"
+echo		"</li>";
+echo		"<li>";
+echo			"Therapie";
+echo			"<ul>";
+echo				"<li><a href="ontspanningstherapie.html">Adem- en Ontspanningstherapie</a></li>";
+echo				"<li><a href="werkwijze.html">Werkwijze</a></li>";
+echo				"<li><a href="vergoedingen.html">Vergoedingen</a></li>";
+echo			"</ul>";
+echo	"</li>";
+echo			"<li>";
+echo			"Resultaten";
+echo			"<ul>";
+echo				"<li><a href="ervaringen.html">Ervaringen</a></li>";
+echo				"<li><a href="reacties.html">Reacties</a></li>";
+			
+echo			"</ul>";
+echo				"<li><a href="logo-ontspanningstherapie.html">Logo</a></li>	";
+echo		"<li><a href="bedrijven.html">Bedrijven</a></li>";
+	
+echo		"<li><a href="contact.html">Contact</a></li>";
+echo		"<ul></nav>";
